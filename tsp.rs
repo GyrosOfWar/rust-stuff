4033143d90e397bbc7a301bb66bac65dd6fc0b08
Martin Tomasi <martin.tomasi@gmail.com>
2014-06-20 20:03:23 +0200

    implemented crossover, mutate methods, bugs still there

--- tsp.rs
+++ tsp.rs
@@ -1,6 +1,7 @@
 extern crate time;
 
-use std::rand::{Rng, task_rng};
+use std::rand::{Rng, task_rng, SeedableRng, StdRng};
+use std::rand::distributions::{IndependentSample, Range};
 use std::collections::HashMap;
 use time::precise_time_ns;
 use std::io::{File, BufferedReader, IoResult};
@@ -86,8 +87,8 @@ impl Tour {
 	}
 
 	fn random_tour<R: Rng>(rng: &mut R, graph: &Graph) -> Tour {
-		let node_count = graph.adj_list.len();
-		let mut tour_nodes: Vec<Node> = range(0, node_count).collect();
+		let node_count = graph.size();
+		let mut tour_nodes = graph.nodes();
 		rng.shuffle(tour_nodes.as_mut_slice());
 		let last: Node = *tour_nodes.last().unwrap();
 		tour_nodes.insert(0, last);
@@ -95,6 +96,57 @@ impl Tour {
 		Tour::new(tour_nodes, tour_weight)
 	}
 
+	fn mutate<R: Rng>(&mut self, rng: &mut R, graph: &Graph, mutationRate: f64) {
+		let size = self.nodes.len() as f64;
+
+		for i in range(0, self.nodes.len()) {
+			let t = rng.gen::<f64>();
+			if t < mutationRate {
+				let j = (rng.gen::<f64>() * size) as uint;
+				self.nodes.as_mut_slice().swap(i, j);
+			}
+		}
+
+		self.total_weight = Tour::calc_tour_weight(&self.nodes, graph);
+		self.fitness = 1.0 / self.total_weight;
+	}
+
+	fn crossover<R: Rng>(&self, other: Tour, graph: &Graph, rng: &mut R) -> Tour {
+		let size = self.nodes.len();
+		let start = (rng.gen::<f64>() * (size as f64)) as uint;
+		let end = (rng.gen::<f64>() * (size as f64)) as uint;
+
+		let mut new_tour: Vec<Node> = Vec::new();
+		new_tour.grow_set(size - 1, &(std::uint::MAX), std::uint::MAX);
+
+		for i in range(0, size) {
+			if start < end && i > start && i < end {
+				//new_tour[i] = self.nodes.get(i);
+				println!("start {}, end {}", start, end);
+				new_tour.grow_set(i, &(std::uint::MAX), *self.nodes.get(i));
+			}
+			else if start > end {
+				if !(i < start && i > end) {
+					*new_tour.get_mut(i) = *self.nodes.get(i);
+				}
+			}
+		}
+
+		for i in range(0, size) {
+			if !new_tour.contains(other.nodes.get(i)) {
+				for j in range(0, new_tour.len()) {
+					if *new_tour.get(j) == std::uint::MAX {
+						*new_tour.get_mut(i) = *other.nodes.get(i);
+						break;
+					}
+				}
+			}
+		}
+
+		let tour_weight = Tour::calc_tour_weight(&new_tour, graph);
+
+		Tour::new(new_tour, tour_weight)
+	}
 }
 
 #[deriving(Show)]
@@ -134,6 +186,7 @@ impl Graph {
 			.map(|(idx, (x, y))| NodePt::new(idx, x * x_max, y * y_max))
 			.take(num_nodes)
 			.collect();
+		println!("num_nodes = {}", num_nodes);
 
 		Graph::from_nodes(points)
 	} 
@@ -152,6 +205,14 @@ impl Graph {
 		}
 	}
 
+	fn size(&self) -> uint {
+		self.adj_list.len()
+	}
+
+	fn nodes(&self) -> Vec<Node> {
+		range(0, self.size()).collect()
+	}
+
 	fn from_file(file_path: &str) -> Graph {
 		let path = Path::new(file_path);
 		let mut file = BufferedReader::new(File::open(&path));
@@ -160,16 +221,27 @@ impl Graph {
 
 		for line in file.lines() {
 			let mut numbers: Vec<f64> = Vec::new();
+			// This could be a lot nicer
 			match line {
 				Ok(val) => {
 					for token in val.as_slice().split(' ') {
-						numbers.push(from_str::<f64>(token).unwrap());
+						if token == "\n" {
+							continue
+						}
+
+						let n = match from_str::<f64>(token) {
+							Some(num) => num,
+							None => continue
+						};
+						numbers.push(n);
 					}
 				},
 				Err(_) => fail!("Failed reading file.")
 			}
-			let point = NodePt::new(i, *numbers.get(0), *numbers.get(1));
-			nodes.push(point);
+			if numbers.len() == 2 {
+				let point = NodePt::new(i, *numbers.get(0), *numbers.get(1));
+				nodes.push(point);
+			}
 			i += 1;
 		}
 
@@ -177,11 +249,29 @@ impl Graph {
 	}
 }
 
-fn main() {
-	let test = "20 14";
-	let split: Vec<f64> = test.split(' ').map(|i| from_str::<f64>(i).unwrap_or(0.0)).collect();
-	println!("{}", NodePt::new(0, *split.get(0), *split.get(1)))
+struct Population {
+	rng: Box<Rng>,
+	graph: Box<Graph>,
+	population: Vec<Tour>
+}
 
-	let mut rng = task_rng();
-	let graph = Graph::random_graph(&mut rng, 10, 200.0, 200.0);
+impl Population {
+	fn new(population_count: uint, graph: &Graph) -> Population {
+		unimplemented!();
+	}
+}
+
+fn main() {
+	let mut rng: StdRng = SeedableRng::from_seed(&[1, 2, 3, 4]);
+	let graph = Graph::random_graph(&mut rng, 5, 200.0, 200.0);
+	let nodes: Vec<Node> = graph.adj_list.keys().map(|x| x.clone()).collect();
+	println!("graph.nodes = {}", nodes);
+
+	let tour = Tour::random_tour(&mut rng, &graph);
+	println!("tour: {}", tour);
+	// let tour1 = Tour::random_tour(&mut rng, &graph);
+	// let tour2 = Tour::random_tour(&mut rng, &graph);
+	// println!("tour 1 = {}\ntour 2 = {}", tour1, tour2);
+	// let child = tour1.crossover(tour2, &graph, &mut rng);
+	// println!("child  = {}", child);
 }
